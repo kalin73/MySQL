0eee9e9d9f058812ad295ec49f98dfaafe553306
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-01-30 19:43:14 +0200

    solution

---
+++ Table Relations/Exercise/Peaks in Rila.sql
@@ -0,0 +1,5 @@
+SELECT m.mountain_range, p.peak_name, p.elevation AS peak_elevation
+FROM mountains m
+JOIN peaks p ON p.mountain_id = m.id
+WHERE m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
