81af34c5d275163972d93f6a6f186554baf794c5
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-02 00:30:47 +0200

    solution

---
+++ Subqueries and JOINs/Lab/Managers.sql
@@ -0,0 +1,8 @@
+SELECT e.employee_id,
+       CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+       e.department_id,
+       d.name                                 AS department_name
+FROM employees e
+         RIGHT JOIN departments d ON d.manager_id = e.employee_id
+ORDER BY e.employee_id
+LIMIT 5;
