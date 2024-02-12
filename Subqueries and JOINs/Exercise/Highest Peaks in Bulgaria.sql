d25e4c264b3e56cdb56ea65fb712deac7b37ee78
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-12 19:15:27 +0200

    solution

---
+++ Subqueries and JOINs/Exercise/Highest Peaks in Bulgaria.sql
@@ -0,0 +1,7 @@
+SELECT c.country_code, m.mountain_range, p.peak_name, p.elevation
+FROM countries c
+JOIN mountains_countries mc ON c.country_code = mc.country_code
+JOIN mountains m ON mc.mountain_id = m.id
+JOIN peaks p ON mc.mountain_id = p.mountain_id
+WHERE c.country_code = 'BG' AND p.elevation > 2835
+ORDER BY p.elevation DESC;
