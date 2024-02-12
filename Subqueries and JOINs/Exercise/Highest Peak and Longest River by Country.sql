8fcde00f91910b86c15a522827f2188a806746c4
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-13 00:16:26 +0200

    solution

---
+++ Subqueries and JOINs/Exercise/Highest Peak and Longest River by Country.sql
@@ -0,0 +1,11 @@
+SELECT c.country_name
+     , MAX(p.elevation) AS highest_peak_elevation
+     , MAX(r.length)    AS longest_river_length
+FROM countries c
+         JOIN mountains_countries mc ON c.country_code = mc.country_code
+         JOIN peaks p ON p.mountain_id = mc.mountain_id
+         JOIN countries_rivers cr ON cr.country_code = c.country_code
+         JOIN rivers r ON r.id = cr.river_id
+GROUP BY c.country_name
+ORDER BY highest_peak_elevation DESC, longest_river_length DESC, c.country_name
+LIMIT 5;
