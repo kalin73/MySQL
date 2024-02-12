7538c3023373f5fc1fd0fc7e7cc410dd7bc4c26b
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-12 19:04:17 +0200

    solution

---
+++ Subqueries and JOINs/Exercise/Employee Summary.sql
@@ -0,0 +1,8 @@
+SELECT e1.employee_id, CONCAT(e1.first_name, ' ', e1.last_name) AS employee_name,
+       CONCAT(e2.first_name, ' ', e2.last_name) AS manager_name, d.name
+FROM employees e1
+JOIN employees e2 ON e1.manager_id = e2.employee_id
+JOIN departments d ON e1.department_id = d.department_id
+WHERE e1.manager_id IS NOT NULL
+ORDER BY e1.employee_id
+LIMIT 5;
