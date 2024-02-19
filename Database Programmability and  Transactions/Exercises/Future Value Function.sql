546d8524a5aed0f02c9ba13a8023989dfa090de2
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-19 19:28:01 +0200

    solution

--- Database Programmability and  Transactions/Exercises/Future Value Function.sql
+++ Database Programmability and  Transactions/Exercises/Future Value Function.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION ufn_calculate_future_value(sum DOUBLE(10, 4), rate DOUBLE(10, 2), years INT)
+CREATE FUNCTION ufn_calculate_future_value(sum DOUBLE(10, 4), rate DOUBLE(10, 4), years INT)
     RETURNS DECIMAL (10, 4)
     DETERMINISTIC
 BEGIN
