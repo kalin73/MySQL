32c5791ab4c1fcf15a23f531361dc122b2b7573b
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-20 01:29:43 +0200

    solution

---
+++ Database Programmability and  Transactions/Exercises/Withdraw Money.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(12, 4))
+BEGIN
+   START TRANSACTION;
+    IF(money_amount < 1 OR
+        (SELECT balance FROM accounts WHERE id = account_id) < money_amount)
+        THEN ROLLBACK;
+        ELSE UPDATE accounts
+             SET balance = balance - money_amount
+             WHERE id = account_id;
+    END IF;
+END
