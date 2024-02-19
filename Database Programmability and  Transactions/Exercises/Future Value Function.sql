c27e574d0081f8ae8083cbd39580b48db804671a
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-19 18:47:28 +0200

    solution

---
+++ Database Programmability and  Transactions/Exercises/Future Value Function.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION ufn_calculate_future_value(sum DOUBLE(10, 4), rate DOUBLE(10, 2), years INT)
+    RETURNS DECIMAL (10, 4)
+    DETERMINISTIC
+BEGIN
+    DECLARE res DECIMAL(10, 4);
+    SET res := sum * POW(rate + 1, years);
+    RETURN res;
+END
