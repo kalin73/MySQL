58d31f0334f6289623ffa0b17c7e3cef29382738
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-21 19:19:21 +0200

    solution

---
+++ DDL-DML Exam Preparation/Buhtig Insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO issues(title, issue_status, repository_id, assignee_id)
+VALUES (CONCAT('Critical Problem With index.db!'), 'open', CEIL(46 * 2 / 3), 14),
+       (CONCAT('Critical Problem With Accelerate.sick!'), 'open', CEIL(47 * 2 / 3), 15),
+       (CONCAT('Critical Problem With index.soshy!'), 'open', CEIL(48 * 2 / 3), 4),
+       (CONCAT('Critical Problem With compile.html!'), 'open', CEIL(49 * 2 / 3), 14),
+       (CONCAT('Critical Problem With Bean.php!'), 'open', CEIL(50 * 2 / 3), 2);
