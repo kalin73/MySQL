66737e906f76740753ea014fa1ad73ea0f648421
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-22 02:06:42 +0200

    solution

---
+++ DDL-DML Exam Preparation/Instagraph Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO comments(content, user_id, post_id)
+VALUES (CONCAT('Omg!', 'ScoreAntigarein', '!This is so cool!'), 1 * 3 / 2, 1),
+       (CONCAT('Omg!', 'HighAsmahow', '!This is so cool!'), 2 * 3 / 2, 2),
+       (CONCAT('Omg!', 'WhoDenoteBel', '!This is so cool!'), 3 * 3 / 2, 3),
+       (CONCAT('Omg!', 'ScoreAntigarein', '!This is so cool!'), 4 * 3 / 2, 4),
+       (CONCAT('Omg!', 'ScoreImmagidefon', '!This is so cool!'), 5 * 3 / 2, 5),
+       (CONCAT('Omg!', 'DarkImmagidsa', '!This is so cool!'), 6 * 3 / 2, 6),
+       (CONCAT('Omg!', 'HighAsmahow', '!This is so cool!'), 7 * 3 / 2, 7),
+       (CONCAT('Omg!', 'RoundAntigaBel', '!This is so cool!'), 8 * 3 / 2, 8),
+       (CONCAT('Omg!', 'BlaImmagiIana', '!This is so cool!'), 9 * 3 / 2, 9),
+       (CONCAT('Omg!', 'BlaImmagiIana', '!This is so cool!'), 10 * 3 / 2, 10);
